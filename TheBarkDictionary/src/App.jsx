81aa7cddf7168c1e34671742b447502e2130188c
Coding <codingmaniac227@gmail.com>
2025-12-30 01:59:39 -0500

    feat(routing): configure nested routes with shared layout

--- TheBarkDictionary/src/App.jsx
+++ TheBarkDictionary/src/App.jsx
@@ -1,12 +1,19 @@
 
 import './App.css'
-import Modal from "./components/Modal.jsx";
+import {Route, Routes} from "react-router-dom";
+import Modal from "./pages/Modal.jsx";
+import MoreInfo from "./pages/MoreInfo.jsx";
+import Webpage from "./components/Webpage.jsx";
 
 function App() {
   return (
     <>
-      <div>The Bark Dictionary</div>
-      <Modal />
+      <Routes>
+          <Route element={<Webpage />}>
+              <Route path="/" element={<Modal />} />
+              <Route path='more-info' element={<MoreInfo />} />
+          </Route>
+      </Routes>
     </>
   )
 }
